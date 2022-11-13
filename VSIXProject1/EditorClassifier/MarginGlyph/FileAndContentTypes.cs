6a5a81139d1bf6c548b41e278b9abdeab571cda6
ryuya11235 <54235921+ryuya1123@users.noreply.github.com>
2022-11-13 16:11:40 +0900

    feat: Add Create a margin glyph.

---
+++ VSIXProject1/EditorClassifier/MarginGlyph/FileAndContentTypes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Text;
+using System.Linq.Expressions;
+
+namespace VSIXProject1.EditorClassifier
+{
+	internal class FileAndContentTypes
+	{
+	}
+
+	internal static class FileAndContentTypeDefinitions
+	{
+		[Export]
+		[Name("hid")]
+		[BaseDefinition("text")]
+		internal static ContentTypeDefinition hidingContentTypeDefinition;
+
+		[Export]
+		[FileExtension(".hid")]
+		[ContentType("hid")]
+		internal static FileExtensionToContentTypeDefinition hiddenFileExtensionDefinition;
+
+		[Export(typeof(IGlyphFactoryProvider))]
+		[Name("TodoGlyph")]
+		[Order(After = "VsTextMarker")]
+		[ContentType("code")]
+		[TagType(typeof(TodoTag))]
+		internal sealed class TodoGlyphFactoryProvider : IGlyphFactoryProvider
+		{
+            public IGlyphFactory GetGlyphFactory(IWpfTextView view, IWpfTextViewMargin margin)
+            {
+                return new TodoGlyphFactory();
+            }
+        }
+
+		[Export(typeof(ITaggerProvider))]
+		[ContentType("code")]
+		[TagType(typeof(TodoTag))]
+		class TodoTaggerProvider : ITaggerProvider
+		{
+			[Import]
+			internal IClassifierAggregatorService AggregatorService;
+
+			public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+			{
+				if(buffer == null)
+				{
+					throw new ArgumentNullException("buffer");
+				}
+				return new TodoTagger(AggregatorService.GetClassifier(buffer)) as ITagger<T>;
+			}
+		}
+	}
+}
---
+++ VSIXProject1/EditorClassifier/MarginGlyph/TodoTagger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace VSIXProject1.EditorClassifier
+{
+
+    internal class TodoTagger : ITagger<TodoTag>
+    {
+        private IClassifier m_classifier;
+        private const string m_searchText = "todo";
+
+        internal TodoTagger(IClassifier classifier)
+        {
+            m_classifier = classifier;
+
+        }
+
+        IEnumerable<ITagSpan<TodoTag>> ITagger<TodoTag>.GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            foreach (SnapshotSpan span in spans)
+            {
+                //look at each classification span \
+                foreach (ClassificationSpan classification in m_classifier.GetClassificationSpans(span))
+                {
+                    //if the classification is a comment
+                    if (classification.ClassificationType.Classification.ToLower().Contains("comment"))
+                    {
+                        //if the word "todo" is in the comment,
+                        //create a new TodoTag TagSpan
+                        int index = classification.Span.GetText().ToLower().IndexOf(m_searchText);
+                        if (index != -1)
+                        {
+                            yield return new TagSpan<TodoTag>(new SnapshotSpan(classification.Span.Start + index, m_searchText.Length), new TodoTag());
+                        }
+                    }
+                }
+            }
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+    }
+
+    internal class TodoTag : IGlyphTag
+    {
+
+    }
+
+
+}
