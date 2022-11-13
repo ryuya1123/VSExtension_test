56df3c750fe375455356f2ec0b55474e1fdd336d
ryuya11235 <54235921+ryuya1123@users.noreply.github.com>
2022-11-13 16:35:07 +0900

    feat: Add  class for customaize text view.

--- VSIXProject1/EditorClassifier/FileAndContentTypes.cs
+++ VSIXProject1/EditorClassifier/FileAndContentTypes.cs
@@ -10,6 +10,8 @@ using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Text;
 using System.Linq.Expressions;
+using System.Windows.Media;
+using System.Windows;
 
 namespace VSIXProject1.EditorClassifier
 {
@@ -58,6 +60,49 @@ namespace VSIXProject1.EditorClassifier
 				}
 				return new TodoTagger(AggregatorService.GetClassifier(buffer)) as ITagger<T>;
 			}
+		}
+
+		[Export(typeof(IWpfTextViewCreationListener))]
+		[ContentType("text")]
+		[TextViewRole(PredefinedTextViewRoles.Document)]
+		internal class testViewCreationListener : IWpfTextViewCreationListener
+		{
+			[Import]
+			internal IEditorFormatMapService FormatMapService = null;
+
+			public void TextViewCreated(IWpfTextView textView)
+			{
+				IEditorFormatMap formatMap = FormatMapService.GetEditorFormatMap(textView);
+
+                ResourceDictionary regularCaretProperties = formatMap.GetProperties("Caret");
+                ResourceDictionary overwriteCaretProperties = formatMap.GetProperties("Overwrite Caret");
+                ResourceDictionary indicatorMargin = formatMap.GetProperties("Indicator Margin");
+                ResourceDictionary visibleWhitespace = formatMap.GetProperties("Visible Whitespace");
+                ResourceDictionary selectedText = formatMap.GetProperties("Selected Text");
+                ResourceDictionary inactiveSelectedText = formatMap.GetProperties("Inactive Selected Text");
+
+                formatMap.BeginBatchUpdate();
+
+                regularCaretProperties[EditorFormatDefinition.ForegroundBrushId] = Brushes.Magenta;
+                formatMap.SetProperties("Caret", regularCaretProperties);
+
+                overwriteCaretProperties[EditorFormatDefinition.ForegroundBrushId] = Brushes.Turquoise;
+                formatMap.SetProperties("Overwrite Caret", overwriteCaretProperties);
+
+                indicatorMargin[EditorFormatDefinition.BackgroundColorId] = Colors.LightGreen;
+                formatMap.SetProperties("Indicator Margin", indicatorMargin);
+
+                visibleWhitespace[EditorFormatDefinition.ForegroundColorId] = Colors.Red;
+                formatMap.SetProperties("Visible Whitespace", visibleWhitespace);
+
+                selectedText[EditorFormatDefinition.BackgroundBrushId] = Brushes.LightPink;
+                formatMap.SetProperties("Selected Text", selectedText);
+
+                inactiveSelectedText[EditorFormatDefinition.BackgroundBrushId] = Brushes.DeepPink;
+                formatMap.SetProperties("Inactive Selected Text", inactiveSelectedText);
+
+                formatMap.EndBatchUpdate();
+            }
 		}
 	}
 }
---
+++ VSIXProject1/EditorClassifier/TextView/ViewPropertyModifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using System.Windows.Media;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+
+namespace VSIXProject1.EditorClassifier.TextView
+{
+    internal class ViewPropertyModifier
+    {
+    }
+}
